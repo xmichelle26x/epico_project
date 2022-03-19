7aac0bf3d4b525a2bcd17b6be2d300bc5a2daedb
Kevin <kevin_vergara.rodriguez@yahoo.es>
2022-03-19 17:52:41 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import MainPage from './pages/mainPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav} from 'react-bootstrap' 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+
+            
+            <Nav className="me-auto">
+       
+                <Link  to="/">Main Page</Link>
+
+   
+              <Link  to="/register">Register</Link>
+
+                <Link  to="/login">Login</Link>
+  
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+        <Routes>
+          <Route path="/" element={ <MainPage/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div> 
   );
 }
 
 export default App;
+ 
