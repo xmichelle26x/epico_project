c6700a19585141ddbcdc16a42aa83c13462ba325
xmichelle26 <xmichelle26x@gmail.com>
2022-03-25 18:32:02 -0500

    Fixes

--- src/pages/elements/navbar.jsx
+++ src/pages/elements/navbar.jsx
@@ -4,25 +4,23 @@ import { Navbar, Container, Nav, Image} from 'react-bootstrap'
 function NavPage(){
     return(
         <Navbar style={{backgroundColor:'#2B2994'}}  expand='lg' >
-            <Container>
-            <Navbar.Brand href="https://epico.gob.ec/" target="blank"><Image style={{height: '50px'}} src="assets/logo_epico.png"></Image></Navbar.Brand>
+            <Container >
+            <Navbar.Brand href="https://epico.gob.ec/" target="blank"><Image style={{height: '40px'}} src="assets/logo_epico.png"></Image></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
+                
                 <Nav className="me-auto navStyle">
                     <LinkContainer to="/">
-                        <Nav.Link>Inicio</Nav.Link>
+                        <a  className="text-nav">Inicio</a>
                     </LinkContainer>
-                    {/* <LinkContainer to="/register">
-                        <Nav.Link>Registro</Nav.Link>
-                    </LinkContainer> */}
                     <LinkContainer to="/application">
-                        <Nav.Link >Solicitud de BECA</Nav.Link>
+                        <a  className="text-nav">Solicitud de BECA</a>
                     </LinkContainer>
                 </Nav>
 
                     <div >
                     <LinkContainer to="/login"> 
-                        <a className="header-button" href="https://epico.gob.ec/ce/auth/login/home">Inicia sesión</a>		            
+                        <a className="header-button">Iniciar sesión</a>		            
                     </LinkContainer>
                     </div>
 
